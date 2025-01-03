d59d1b072bbe436a7e93ae62b40d98f769518874
matth <deaconmp@lafayette.edu>
2025-01-03 12:38:15 -0500

    Commit fourth group of files

---
+++ Pipeline_Folder_2/Daniel_JSON_Files_Elmer/Dummy_Pipeline2.0.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:69a8b6cfd7c8b9a3023e742dac35da491b1dfbb93d0ca982fbb909b937e0dfc9
+size 2194
---
+++ Pipeline_Folder_2/Daniel_JSON_Files_Elmer/finish_window.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:387adf3935df9a637cbdf0f3a8e70b87e220c422d7687fe936a5ad01f9e97111
+size 1433
