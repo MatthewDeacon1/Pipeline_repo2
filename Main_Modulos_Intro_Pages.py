d2e8d89bce7edd8d3f2f8ee9798d3fb2cbac6aa0
matth <deaconmp@lafayette.edu>
2025-01-03 11:40:50 -0500

    Commit fourth group of files

--- Main_Modulos_Intro_Pages.py
+++ Main_Modulos_Intro_Pages.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a3142eabf7761b3b619badfba3e900bf1b89ea60844bf163db5d43cf18ed0a1
-size 34059
+oid sha256:696cfde876826594328f58e75af33593db403939edd25690117be72ebaddd5ca
+size 34215
